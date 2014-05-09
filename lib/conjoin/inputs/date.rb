7fbd09699fba705ccd93a2b30928cf3672c8016e
Adam Nazar <anazar@gmail.com>
2014-05-09 00:44:22 -0700

    added append button option to input date

--- lib/conjoin/inputs/date.rb
+++ lib/conjoin/inputs/date.rb
@@ -4,7 +4,25 @@ module Conjoin
       def display
         options[:date] = true
         options[:value] = R18n.l options[:value]
-        super
+
+        append_button       = options.delete :append_button
+
+        content = super
+
+        if append_button
+          content = mab do
+            div class: 'input-group' do
+              text! content
+              div class: 'input-group-btn' do
+                button class: 'btn btn-primary', type: append_button[:type] || 'button', 'on-click-get' => append_button[:href] do
+                  text append_button[:text]
+                end
+              end
+            end
+          end
+        end
+
+        content
       end
     end
   end
